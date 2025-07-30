ba626346140dbd7bcb307f428014cd7f3cd2a628
pradeep <pradeepbarik87@gmail.com>
2025-07-31 00:43:26 +0530

    Fafd

--- app/components/mobile/need-help-btn/index.tsx
+++ app/components/mobile/need-help-btn/index.tsx
@@ -3,7 +3,7 @@ import { CSSProperties } from "react";
 import { BiPhone } from "react-icons/bi";
 const NeedHelpBtn=({style={}}:{style?:CSSProperties})=>{
     return (
-        <Link href={"/contact-us"}  className="button flex items-center fixed -right-8 bottom-10" data-variant="outlined" data-color="secondary" style={{transform:"rotate(-90deg)",...style}}><BiPhone/>Need Help?</Link>
+        <Link href={"/contact-us"}  className="button flex items-center fixed -right-6 bottom-14" data-variant="outlined" data-color="secondary" style={{transform:"rotate(-90deg)",...style}}><BiPhone/>Need Help?</Link>
     )
 }
 export default NeedHelpBtn;
