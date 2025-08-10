3e98d9a58be9d6edfe8688f8f3ed366c62b8f55e
pradeep <pradeepbarik87@gmail.com>
2025-08-10 12:31:22 +0530

    dsdd

--- constants/site-config.js
+++ constants/site-config.js
@@ -4,4 +4,4 @@ export const site_url="http://careipro.com";
 export const site_name='Careipro';
 export const default_latlng={lat:21.058985,lng:86.4912713};
 export const google_map_api_key='AIzaSyCnnN5cUvpKCIxNOchrFr7VEJBSMXgaUWg';
-export const support_no="819709283";
+export const support_no="8197909283";
