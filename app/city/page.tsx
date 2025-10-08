2a5a8f3e1e1327076ff165563092067db514f497
pradeep <pradeepbarik87@gmail.com>
2025-10-08 23:04:27 +0530

    footer menu

--- app/city/page.tsx
+++ app/city/page.tsx
@@ -38,7 +38,7 @@ const CityHomePage = ({ searchParams }: TProps) => {
           state: searchParams.state,
           city: searchParams.city,
         }} />
-        <FooterMenu cookies={{state:searchParams.state,city:searchParams.city}} />
+        <FooterMenu cookies={{state:searchParams.state,city:searchParams.city,userSecreateKey:cookies[userSecreateKey]}} />
         {/* <NeedHelpBtn style={{ bottom: '25vh' }} /> */}
       </>
     )
