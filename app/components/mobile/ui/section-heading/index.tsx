240f585fd7d0deaebac52c13d88c2133f5f5af66
pradeep <pradeepbarik87@gmail.com>
2025-12-15 23:08:13 +0530

    dfasfds

--- app/components/mobile/ui/section-heading/index.tsx
+++ app/components/mobile/ui/section-heading/index.tsx
@@ -7,9 +7,9 @@ const SectionHeading = ({ heading, className = "",children }: { heading: string,
     </h2>
 }
 export const SectionSubHeading = ({ heading, className = "",children }: { heading: string, className?: string,children?:ReactNode }) => {
-    return <h3 className={`fs-16 font-semibold px-2 py-2 flex items-center section-heading ${className}`}>
+    return <h2 className={`fs-16 font-semibold px-2 py-2 flex items-center section-heading ${className}`}>
         {heading}
         {children}
-    </h3>
+    </h2>
 }
 export default SectionHeading;
